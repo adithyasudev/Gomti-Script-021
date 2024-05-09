d7de484fe98af41de5800c764a7b26c7e39fdff9
tusharbisht16 <tusharbisht0216@gmail.com>
2024-05-09 23:58:22 +0530

    making redux store

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 
 import './App.css'
 import Bluebox from './Components/Bluebox'
+import Services from './pages/Services'
 
 function App() {
   
 
   return (
     <>
-      <p>Hello</p>
+      <Services/>
   
     </>
   )
---
+++ src/redux/reduxStore.tsx
@@ -0,0 +1,33 @@
+// import axios from "axios";
+
+// let Login_user = "LOGIN_USER";
+// let Login_admin = "LOGIN_ADMIN";
+
+// let intialUserDetails={
+//     email:"",
+//     password:""
+
+// }
+ 
+
+
+//  async function fetchData{
+//     try{
+//       let res = await axios.get("https://gomti-script-021.onrender.com/user");
+      
+    
+//     }
+//     catch{
+
+//     }
+//  }
+
+ 
+
+// const loginreducer =(state = intialUserDetails,action)=>{
+// switch (action.type){
+//  case "LOGIN_USER":
+
+//  case "LOGIN_ADMIN":
+// }
+// }
