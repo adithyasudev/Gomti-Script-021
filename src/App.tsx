5173644019bccfd568e7c2808cafc4f6f2570476
Sartik0709 <sharmasartik@gmail.com>
2024-05-11 15:33:14 +0530

    mainpush

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 
 
 import './App.css'
+<<<<<<< Updated upstream
+=======
+import Contectus from './Components/Contectus'
+import  InitialFocus from './Components/Login-form'
+import Events from './Pages/Events'
+
+import Portfolio from './Pages/Portfolio'
+
+
+>>>>>>> Stashed changes
 
 function App() {
   
 
   return (
     <>
+<<<<<<< Updated upstream
       <p>Hello</p>
       
+=======
+     
+>>>>>>> Stashed changes
     </>
   )
 }
---
+++ src/Components/Event2.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Style from'./Sartik.module.css'
+
+
+interface eventcard{
+    id:number;
+    image:string;
+    title:string;
+    discription:string;
+    date:string;
+}
+
+const EventCard = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://gomti-script-021.onrender.com/events');
+        setEvents(response.data);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    // <figure className={Style.eventcard}>
+    //   {events.map((event:eventcard) => (
+       
+    //     <>   <img src={event.image} alt={event.title}  style={{gridArea:'{event.image}'}} className={Style.carditem}/>
+    //          <figcaption>
+    //               <h5>{event.title}</h5>
+    //               <p>{event.discription}</p>
+    //               <p>{event.date}</p>
+    //           </figcaption>
+    //     </>
+      <div>
+         {events.map((event:eventcard) => (
+          <div style={{backgroundImage:`url(${event.image})`}}>
+
+          </div>
+
+        
+         ))}
+
+</div>
+
+
+
+   
+  );
+};
+
+export default EventCard;
