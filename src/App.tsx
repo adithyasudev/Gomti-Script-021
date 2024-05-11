3723f105abffb3f685e72123d6d1d756fe98daf0
Sartik0709 <sharmasartik@gmail.com>
2024-05-11 23:31:08 +0530

    portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 import './App.css'
 import Portfolio1 from './Components/Portfolio1'
 import InitialFocus from './Components/Login-form'
+import Portfolio from './Pages/Portfolio'
 function App() {
   
 
   return (
     <>
     <InitialFocus />
-    <Portfolio1 />
+    <Portfolio />
     </>
   )
 }
