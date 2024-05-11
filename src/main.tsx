8a9f559d5412795e0b6e0f1eee266132e511c3e1
Adithya sudev <adithyasudev28@gmail.com>
2024-05-12 01:35:31 +0530

    full workk

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import store from './redux/reduxStore.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
+
+<ChakraProvider >
     <App />
-  </React.StrictMode>,
+
+    </ChakraProvider>
+  </Provider>
+    
+    
+  
 )
