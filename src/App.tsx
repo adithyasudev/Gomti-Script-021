f07c7d85fd9c7e3a46b34a80a6ab91da8b4ca844
tusharbisht16 <tusharbisht0216@gmail.com>
2024-05-12 17:08:32 +0530

    latest commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
+import "./App.css";
+import Services from "./Pages/Services";
 
-
-
-
-import './App.css'
-import Services from './pages/Services'
-
-
-
-import './App.css';
-import { ContactUs } from './pages/ContactUs';
-import CareerPage from './pages/CareerPage';
-import Events from './pages/Events';
-import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
-import { Route, Routes } from 'react-router';
-import { About } from './pages/About';
+import "./App.css";
+import { ContactUs } from "./Pages/ContactUs";
+import CareerPage from "./Pages/CareerPage";
+import Events from "./Pages/Events";
+import Home from "./Pages/Home";
+import Portfolio from "./Pages/Portfolio";
+import { Route, Routes } from "react-router";
+import { About } from "./Pages/About";
 function App() {
   return (
     <>
-<Routes>
-<Route path="/" element={<Home />}/>
-<Route path="/about" element={<About />} />
-<Route path="/contactus" element={<ContactUs/>}/>
-<Route path="/career" element={<CareerPage/>}/>
-<Route path="/services" element={<Services/>} />
-<Route path="/portfolio" element={<Portfolio/>} />
-<Route path="/events" element={<Events/>} />
-</Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/career" element={<CareerPage />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/events" element={<Events />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
--- src/Components/SIdebar.tsx
+++ src/Components/SIdebar.tsx
@@ -1,62 +1,55 @@
 import {
-    Drawer,
-    DrawerBody,
-   Button,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerFooter,
-    DrawerContent,
-    DrawerCloseButton,
-    useDisclosure 
+  Drawer,
+  DrawerBody,
+  Button,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerFooter,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 
-  } from '@chakra-ui/react'
+import { useDispatch, useSelector } from "react-redux";
 
-
-  import { useDispatch, useSelector } from "react-redux"
-
-
-
-import AdminDashboard from '../pages/AdminDashboard'
+import AdminDashboard from "../Pages/AdminDashboard";
 
 import { ADMIN_LOGOUT, LOGOUT_REQUEST } from "../redux/reducers";
 
-
-
 export function Sidebar() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const adminLoggedin = useSelector(state=>state.admin.isAuthenticated)
-    const userloggedin = useSelector(state=>state.user.isAuthenticated)
-    const dispatch = useDispatch();
-    const handleClick = () => {
-      onOpen()
-    }
-  
- 
-  
-    return (
-      <>
-      
-          <Button
-            onClick={() => handleClick()}
-         
-            m={4}
-          >DashBoard</Button>
-      
-  
-        <Drawer onClose={onClose} isOpen={isOpen} size="md">
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader></DrawerHeader>
-            <DrawerBody>
-             {adminLoggedin&&(<AdminDashboard/>)}  
-            </DrawerBody>
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const adminLoggedin = useSelector((state) => state.admin.isAuthenticated);
+  const userloggedin = useSelector((state) => state.user.isAuthenticated);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    onOpen();
+  };
 
-            <DrawerFooter>
-            <button  onClick={()=>{dispatch({type:LOGOUT_REQUEST});dispatch({type:ADMIN_LOGOUT})}}>Logout</button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </>
-    )
-  }
+  return (
+    <>
+      <Button onClick={() => handleClick()} m={4}>
+        DashBoard
+      </Button>
+
+      <Drawer onClose={onClose} isOpen={isOpen} size="md">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader></DrawerHeader>
+          <DrawerBody>{adminLoggedin && <AdminDashboard />}</DrawerBody>
+
+          <DrawerFooter>
+            <button
+              onClick={() => {
+                dispatch({ type: LOGOUT_REQUEST });
+                dispatch({ type: ADMIN_LOGOUT });
+              }}
+            >
+              Logout
+            </button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
