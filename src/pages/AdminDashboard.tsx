ab3d5ded5b51f1b234cce9b5fe932aed826d52e5
Sartik0709 <sharmasartik@gmail.com>
2024-05-14 00:54:49 +0530

    finalcommit

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,12 +1,15 @@
 
+import { Stack } from '@chakra-ui/react'
 import AdminNav from '../Components/AdminNav'
 import Adminbody from '../Components/Adminbody'
 
 const AdminDashboard = () => {
   return (
     <>
+    <Stack spacing={2}>
     <AdminNav/>
     <Adminbody/>
+    </Stack>
     </>
   )
 }
